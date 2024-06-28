8ab02a101ce0d2ecd684c75777c06f85be788af1
Ali Munzer <ali.sy.9777@gmail.com>
2024-06-28 03:18:29 +0300

    Add the logic to receive data form an array
    set a function taht receive data from an array of objects

--- script/script.js
+++ script/script.js
@@ -1,25 +1,80 @@
-const myLibrary = []
-
-function book(title, auther, pages) {
+function book(title, auther, pages, read) {
   this.title = title
   this.auther = auther
-  this.read = true
+  this.read = read
   this.pages = pages
 }
-const addBook = document.querySelector('#addBook')
-addBook.onclick = () => {
-  addBookToLibrary()
-}
-function addBookToLibrary() {}
-const dialog = document.querySelector('dialog')
-const showButton = document.querySelector('dialog + button')
-const closeButton = document.querySelector('dialog button')
-// "Show the dialog" button opens the dialog modally
-showButton.addEventListener('click', () => {
-  dialog.showModal()
-})
+// dialog logic
+;(() => {
+  const dialog = document.querySelector('dialog')
+  const showButton = document.querySelector('dialog + button')
+  const closeButton = document.querySelector('dialog button')
+  // "Show the dialog" button opens the dialog modally
 
-// "Close" button closes the dialog
-closeButton.addEventListener('click', () => {
-  dialog.close()
-})
+  showButton.addEventListener('click', () => {
+    dialog.showModal()
+  })
+
+  // "Close" button closes the dialog
+  closeButton.addEventListener('click', () => {
+    dialog.close()
+  })
+})()
+
+const addBooksToLibrary = () => {
+  // looping thorgh myLibrary array
+  for (book in myLibrary) {
+    // creat a new div and appand it to popular div
+    let books = document.getElementsByClassName('populer')
+    let bookCard = document.createElement('div')
+    bookCard.classList.add('bookCard')
+    books[0].appendChild(bookCard)
+
+    // adding the book information to the new created div from the object in myLibrary array
+    if (book.read === true) {
+      readValue = 'Yes'
+    } else {
+      readValue = 'No'
+    }
+
+    bookCard.innerHTML = `
+              <img
+                src="/assets/books/istockphoto-1166419449-1024x1024.jpg"
+                alt=""
+                class="book-img"
+              />
+              <div class="flexWraper">
+                <div class="book-title">${book.title}</div>
+                <div class="rate">${book.pages}</div>
+              </div>
+              <div class="flexWraper">
+                <div class="auther">${book.auther}</div>
+                <div class="download-times">${readValue}</div>
+              </div>
+            `
+  }
+}
+
+const book1 = new book('ads', 'aa', 'qwe', true)
+
+const book2 = new book('22', '22', '2', true)
+
+const myLibrary = [
+  book1,
+  book2,
+  book1,
+  book2,
+  book1,
+  book2,
+  book1,
+  book2,
+  book1,
+  book2,
+  book1,
+  book2,
+
+  book1,
+  book2,
+]
+console.log(myLibrary)
+addBooksToLibrary()
