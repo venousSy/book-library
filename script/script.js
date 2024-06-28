fa75132965c0dc995a7908682499e6adfa27ffdc
Ali Munzer <ali.sy.9777@gmail.com>
2024-06-28 16:49:28 +0300

    fix a bug with the lojic where the browser couldn't read the array values of mylibrary
    the for in loop didn't work propely so we change it to for each

--- script/script.js
+++ script/script.js
@@ -4,56 +4,6 @@ function book(title, auther, pages, read) {
   this.read = read
   this.pages = pages
 }
-// dialog logic
-;(() => {
-  const dialog = document.querySelector('dialog')
-  const showButton = document.querySelector('dialog + button')
-  const closeButton = document.querySelector('dialog button')
-  // "Show the dialog" button opens the dialog modally
-
-  showButton.addEventListener('click', () => {
-    dialog.showModal()
-  })
-
-  // "Close" button closes the dialog
-  closeButton.addEventListener('click', () => {
-    dialog.close()
-  })
-})()
-
-const addBooksToLibrary = () => {
-  // looping thorgh myLibrary array
-  for (book in myLibrary) {
-    // creat a new div and appand it to popular div
-    let books = document.getElementsByClassName('populer')
-    let bookCard = document.createElement('div')
-    bookCard.classList.add('bookCard')
-    books[0].appendChild(bookCard)
-
-    // adding the book information to the new created div from the object in myLibrary array
-    if (book.read === true) {
-      readValue = 'Yes'
-    } else {
-      readValue = 'No'
-    }
-
-    bookCard.innerHTML = `
-              <img
-                src="assets/books/istockphoto-1166419449-1024x1024.jpg"
-                alt=""
-                class="book-img"
-              />
-              <div class="flexWraper">
-                <div class="book-title">${book.title}</div>
-                <div class="rate">${book.pages}</div>
-              </div>
-              <div class="flexWraper">
-                <div class="auther">${book.auther}</div>
-                <div class="download-times">${readValue}</div>
-              </div>
-            `
-  }
-}
 
 const book1 = new book('ads', 'aa', 'qwe', true)
 
@@ -76,5 +26,55 @@ const myLibrary = [
   book1,
   book2,
 ]
-console.log(myLibrary)
+// dialog logic
+;(() => {
+  const dialog = document.querySelector('dialog')
+  const showButton = document.querySelector('dialog + button')
+  const closeButton = document.querySelector('dialog button')
+  // "Show the dialog" button opens the dialog modally
+
+  showButton.addEventListener('click', () => {
+    dialog.showModal()
+  })
+
+  // "Close" button closes the dialog
+  closeButton.addEventListener('click', () => {
+    dialog.close()
+  })
+})()
+
+const addBooksToLibrary = () => {
+  // creat a new div and appand it to popular div
+  const addTheBooks = (boock) => {
+    let books = document.getElementsByClassName('populer')
+    let bookCard = document.createElement('div')
+    bookCard.classList.add('bookCard')
+    books[0].appendChild(bookCard)
+
+    // adding the book information to the new created div from the object in myLibrary array
+    if (boock.read === true) {
+      readValue = 'Yes'
+    } else {
+      readValue = 'No'
+    }
+
+    bookCard.innerHTML = `<img
+                src="assets/books/istockphoto-1166419449-1024x1024.jpg"
+                alt=""
+                class="book-img"
+              />
+              <div class="flexWraper">
+                <div class="book-title">${boock.title}</div>
+                <div class="rate">${boock.pages}</div>
+              </div>
+              <div class="flexWraper">
+                <div class="auther">${boock.auther}</div>
+                <div class="download-times">${readValue}</div>
+              </div>
+            `
+  }
+  // looping thorgh myLibrary array
+  myLibrary.forEach(addTheBooks)
+}
+
 addBooksToLibrary()
